36896747c98b3ff03b66f3d6cef3089b1055156a
SadiqNahmetov <sadigfn@code.edu.az>
2022-10-02 16:12:14 +0400

    ASP.NET fixed Homework

---
+++ ASP-intro/ASP-intro/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_intro.Controllers
+{
+    public class ContactController 
+    {
+        public string Index()
+        {
+            return "Contact page";
+        }
+    }
+}
---
+++ ASP-intro/ASP-intro/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index(int id, string name)
+        //{
+        //    return "Home page" + "-" + id + "-" + name;
+        //}
+
+        public IActionResult Index(int id, string name)
+        {
+            //return Json(new { name = "Seid", surname="Nuraliyev"});
+            //return Content("Hello P130!");
+            return View();
+        }
+
+        public string FullData( string name,string surname, int age)
+        {
+            return $"Name-{name}; Surname-{surname}-Age-{age}";
+        }
+    }
+}
